130696262243d93d1f9e2930db277a7a91664627
MaxenceGumiero <117424624+MaxouJS@users.noreply.github.com>
2023-03-01 12:17:35 +1100

    Updating controls

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import SampleScene from './components/scenes/SampleScene';
 
 export default function App() {
   return (
-    <div className='h-screen w-screen'>
-      <SampleScene />
+    <div className='flex items-center justify-center h-screen w-screen'>
+      <div className='h-screen w-screen'>
+        <SampleScene />
+      </div>
     </div>
   )
 }
