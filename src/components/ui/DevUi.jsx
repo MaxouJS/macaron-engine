714c8b61722d3371997821481e9396a0113ff5f4
MaxenceGumiero <117424624+MaxouJS@users.noreply.github.com>
2023-03-02 20:54:52 +1100

    Update DevUi.jsx

--- src/components/ui/DevUi.jsx
+++ src/components/ui/DevUi.jsx
@@ -1,12 +1,16 @@
 export default function DevUi() {
   return (
-    <div className='flex items-center justify-center h-full w-full'>
-      <div className='absolute top-0 bg-red-400/25 w-full h-[25vh]' />
-      <div className='absolute mid-0 bg-green-400/25 w-full h-[50vh]' />
-      <div className='absolute bottom-0 bg-red-400/25 w-full h-[25vh]' />
+    <div className='flex items-center justify-center h-full w-full text-xs text-black/25 select-none'>
+      <div className='absolute top-0 bg-red-400/25 w-full h-[25vh] p-2'>Background</div>
+      <div className='absolute top-[25vh] bg-yellow-400/25 w-full h-[25vh] p-2'>Middle ground</div>
+      <div className='absolute bottom-[25vh] bg-green-400/25 w-full h-[25vh] p-2'>Middle ground</div>
+      <div className='absolute bottom-0 bg-yellow-400/25 w-full h-[25vh] p-2'>Foreground</div>
+      <div className='absolute top-[25vh] bg-green-400/50 w-full h-px' />
+      <div className='absolute bottom-[25vh] bg-green-400/50 w-full h-px' />
       <div className='absolute bg-blue-400/50 h-full w-px' />
       <div className='absolute bg-green-400/50 w-full h-px' />
-      <div className='absolute bg-red-400 h-1 w-1' />
+      <div className='absolute bg-red-400 h-2 w-2' />
+      <p className='absolute'>Horizon Line</p>
     </div>
   )
 }
